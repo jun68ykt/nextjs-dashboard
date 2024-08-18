5c791c169c2c034ea1028c412af886fa60611569
jun68ykt <jun68yk+t@gmail.com>
2024-08-18 20:22:41 +0900

    Revert "🔥 Unuse fetching functions"
    This reverts commit 9e06e25fcdb15e9a0d1d5c731fa32ca417669f16.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,16 +2,12 @@ import { Card } from '@/app/ui/dashboard/cards'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
+import { fetchCardData } from '@/app/lib/data'
 import { Suspense } from 'react'
 import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
 
 export default async function Page() {
-  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = {
-    numberOfCustomers: 0,
-    numberOfInvoices: 0,
-    totalPaidInvoices: '',
-    totalPendingInvoices: ''
-  }
+  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData()
 
   return (
     <main>
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,9 +2,10 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import Image from 'next/image'
 import { lusitana } from '@/app/ui/fonts'
+import { fetchLatestInvoices } from '@/app/lib/data'
 
 export default async function LatestInvoices() {
-  const latestInvoices: any[] = []
+  const latestInvoices = await fetchLatestInvoices()
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
