a7b71ab507a9b50352187e8122728bcf89c29f97
jun68ykt <jun68yk+t@gmail.com>
2024-08-18 12:36:07 +0900

    🔥 Remove prop `revenue` of `<RevenueChart />`

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,10 +2,9 @@ import { Card } from '@/app/ui/dashboard/cards'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
   const latestInvoices = await fetchLatestInvoices()
   const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData()
 
@@ -19,7 +18,7 @@ export default async function Page() {
         <Card title="Total Customers" value={numberOfCustomers} type="customers" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <RevenueChart />
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
